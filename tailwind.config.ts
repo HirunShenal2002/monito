392e93dc417d80f0f31bc66567bd9c1fa78339a7
Hirun Shenal Nethmina <115377260+HirunShenal2002@users.noreply.github.com>
2024-10-17 06:05:22 +0000

    Update tailwind.config.ts with extended theme colors and font families

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,31 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#003459",
+        "secondary": "#F7DBA7",
+        "state-red": "#FF564F",
+        "state-green": "#34C759",
+        "state-orange": "#FF912C",
+        "state-blue": "#00A7E7",
+        "neutral-100": "#00171F",
+        "neutral-80": "#242B33",
+        "neutral-60": "#667479",
+        "neutral-40": "#99A2A5",
+        "neutral-20": "#CCD1D2",
+        "neutral-10": "#EBEEEF",
+        "neutral-00": "#FDFDFD",
+      },
+      fontFamily: {
+        "Gilroy-Bold": ["Gilroy-Bold", "sans-serif"],
+        "Gilroy-Medium": ["Gilroy-Medium", "sans-serif"],
+        "Gilroy-Regular": ["Gilroy-Regular", "sans-serif"],
+        "Gilroy-SemiBold": ["Gilroy-SemiBold", "sans-serif"]
+      },
+    }
+  },
   plugins: [],
 };
 export default config;
