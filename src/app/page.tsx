32b5b9678616b129dfdbe728e2ecdac90753bb04
Hirun Shenal Nethmina <115377260+HirunShenal2002@users.noreply.github.com>
2024-10-19 18:01:09 +0000

    banner added for large devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@
 import Button from "@/components/button";
 import Footer from "@/components/footer";
 import ProductCard from "@/components/productCard";
+import { Image } from "@nextui-org/react";
 import { useEffect, useState } from "react";
-import { BsChevronRight } from "react-icons/bs";
+import { BsChevronRight, BsPlayCircle } from "react-icons/bs";
 import { IoIosPlayCircle } from "react-icons/io";
 
 export default function Home() {
@@ -48,10 +49,10 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="px-10 xl:p-0">
+      <div className="px-10 xl:p-0 max-w-[1200px] mx-auto">
 
         {/* pet section */}
-        <div className="max-w-[1200px] mx-auto my-10">
+        <div className="my-10">
           <div className="flex flex-col gap-y-10">
             <div className="flex justify-between items-end">
               <div className="flex flex-col text-primary">
@@ -62,14 +63,16 @@ export default function Home() {
             </div>
             <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
               {pets.map((pet, index) => (
-                <ProductCard key={index} product={pet} isPetsCard={true}/>
+                <ProductCard key={index} product={pet} isPetsCard={true} />
               ))}
             </div>
           </div>
         </div>
 
+        <Banner bannerImage="/imgs/homeBanner1.png" title="One more friend" subtitle="Thousands more fun!" description="Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!" button1="View Into" button2="Explore Now" colour1="#003459" colour2="#FCEED5" colour3="#002A48" isImageLeft={true} />
+
         {/* product section */}
-        <div className="max-w-[1200px] mx-auto my-10">
+        <div className="my-10">
           <div className="flex flex-col gap-y-10">
             <div className="flex justify-between items-end">
               <div className="flex flex-col text-primary">
@@ -85,9 +88,69 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <Banner bannerImage="/imgs/homeBanner2.png" title="Adoption" subtitle="We need help. so do they." description="Adopt a pet and give it a home,
+it will be love you back unconditionally." button1="View Into" button2="Explore Now" colour1="#FFB775" colour2="#FCEED5" colour3="#FFE7BA" isImageLeft={false} />
+
       </div>
 
       <Footer />
     </>
+  );
+}
+
+
+function Banner({
+  bannerImage,
+  title,
+  subtitle,
+  description,
+  button1,
+  button2,
+  colour1,
+  colour2,
+  colour3,
+  isImageLeft
+}: {
+  bannerImage: string,
+  title: string,
+  subtitle: string,
+  description: string,
+  button1: string,
+  button2: string,
+  colour1: string,
+  colour2: string,
+  colour3: string,
+  isImageLeft: boolean
+}) {
+  return (
+    <div className={`w-full lg:h-[380px] relative flex justify-between flex-col-reverse rounded-2xl overflow-clip px-20 ${isImageLeft ? "lg:flex-row" : "lg:flex-row-reverse"}`} style={{ backgroundColor: colour1 }}>
+
+      {isImageLeft ? (
+        <>
+          <div className={`w-[782.29px] h-[635px] absolute top-0 right-0 translate-x-1/4 -translate-y-1/4 z-0 rounded-[99px] rotate-[35deg]`} style={{ backgroundColor: colour2 }}></div>
+          <div className={`w-[782.29px] h-[635px] absolute top-0 left-0 -translate-x-1/4 translate-y-1/4 z-0 rounded-[99px] rotate-[30deg]`} style={{ backgroundColor: colour3 }}></div>
+        </>) : (
+        <>
+          <div className={`w-[782.29px] h-[635px] absolute top-0 left-0 -translate-x-1/3 -translate-y-1/3 z-0 rounded-[99px] rotate-[45deg]`} style={{ backgroundColor: colour2 }}></div>
+          <div className={`w-[782.29px] h-[635px] absolute top-0 right-0 translate-x-1/3 translate-y-1/3 z-0 rounded-[99px] rotate-[45deg]`} style={{ backgroundColor: colour3 }}></div>
+        </>
+      )}
+
+      <Image className="w-full h-full object-cover z-10" src={bannerImage} alt={title} />
+
+      <div className={`w-full lg:w-1/2 flex flex-col justify-center gap-y-5 text-primary z-10 max-w-[400px] p-5 ${isImageLeft ? "text-right" : ""}`}>
+        <h3 className="text-5xl font-bold">{title}</h3>
+        <p className="text-3xl font-semibold">{subtitle}</p>
+        <p>{description}</p>
+
+        <div className={`w-full flex items-center gap-x-5 ${isImageLeft ? "justify-end" : ""}`}>
+          <Button text={button1} color="outlined" icon={<BsPlayCircle />} type="iconLeft" />
+          <Button text={button2} color="primary1" />
+        </div>
+
+      </div>
+
+    </div>
   );
 }
